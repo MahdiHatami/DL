dd9c7d22acf404948118de318333367cfa339d46
Mahdi Hatami <metis@Mahdis-iMac.local>
2018-02-21 15:14:44 +0300

    add logistic and softmax

--- LinearRegression/linearRegression.jl
+++ LinearRegression/linearRegression.jl
@@ -9,22 +9,10 @@ predict(w,x) = w[1]*x .+ w[2]
 
 loss(w,x,y) = mean(abs2, y - predict(w, x))
 
-lossgradient = grad(loss)
-
-function train(w, data; lr=.1)
-    for (x,y) in data
-        dw = lossgradient(w, x, y)
-    	for i in 1:length(w)
-    	    w[i] -= lr * dw[i]
-    	end
-    end
-    return w
-end
-
 w = Any[ 0.1*randn(1,13), 0.0 ]
 
-for i=1:10
+for i=1:15
     train(w, [(x,y)])
     println(loss(w,x,y))
 end
-display(plot(x', [y;predict(w,x)]', seriestype=:scatter, title="Data"))
+# display(plot(x', [y;predict(w,x)]', seriestype=:scatter, title="Data"))
--- SoftMax/soft.jl
+++ SoftMax/soft.jl
@@ -1,26 +1,17 @@
-using Knet, Plots, Images
-using ASTInterpreter2
+using Knet
 
 include(Knet.dir("data","mnist.jl"))
-
 xtrn, ytrn, xtst, ytst = mnist()
 dtrn = minibatch(xtrn, ytrn, 100)
 dtst = minibatch(xtst, ytst, 100)
 
-function predict(w, x)
-    # mat is needed to convert the (28,28,1,N) x array to a (784,N) matrix so it can be used in matrix multiplication.
-    return 1.0 ./ (1.0 + exp(-(w[1]*mat(x) + w[2])))
-end
+predict(w,x) = w[1]*mat(x) .+ w[2]
 
-# nll computes the negative log likelihood of your predictions compared to the correct answers.
-# loss(w,x,y) = nll(predict(w,x), y)
-function loss(w,x,y)
-    return mean(y .* log.(predict(w,x)) + (1 .- y) .* log.(1 .- predict(w,x)))
-end
+loss(w,x,ygold) = nll(predict(w,x), ygold)
 
 lossgradient = grad(loss)
 
-function train(w, data, lr=.1)
+function train(w, data; lr=.1)
     for (x,y) in data
         dw = lossgradient(w, mat(x), y)
         for i in 1:length(w)
@@ -34,8 +25,7 @@ w = Any[ 0.1f0*randn(Float32,10,784), zeros(Float32,10,1) ]
 
 println((:epoch, 0, :trn, accuracy(w,dtrn,predict), :tst, accuracy(w,dtst,predict)))
 
-for epoch=1:10
+for epoch=1:3
     train(w, dtrn; lr=0.5)
     println((:epoch, epoch, :trn, accuracy(w,dtrn,predict), :tst, accuracy(w,dtst,predict)))
 end
-d
